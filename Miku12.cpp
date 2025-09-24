5d9812a1752e71719b90718551ae2aab1ee506f7
Miku Hatsune <game.app13579@gmail.com>
2025-09-24 20:45:45 +0300

    Update Miku12.cpp

--- Miku12.cpp
+++ Miku12.cpp
@@ -1,14 +1,13 @@
-
 #include <iostream>
 #include <string>
 
-int main() {
-    setlocale(LC_ALL, "Russian");
+int main(){
+   
+    setlocale(LC_ALL,"Russian");
     std::string name;
-
-    std::cout << "Введите ваше имя: ";
-    std::getline(std::cin, name);  // Используем getline для считывания строки с пробелами
-    std::cout << "Вы ввели имя: " << name << std::endl;
+    std::cin >> name;
+    std::cout << "РџСЂРёРІРµС‚" << ", " << name << "!";
 
     return 0;
+    
 }
