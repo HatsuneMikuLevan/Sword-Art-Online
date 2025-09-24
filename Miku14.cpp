4d1494a7e55c144b1d078fc57c5a8faa5cb08544
Miku Hatsune <game.app13579@gmail.com>
2025-09-24 21:10:44 +0300

    Add files via upload

---
+++ Miku14.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+#include <iostream> 
+int main() {
+    int num1, num2, num3;
+    std::cin >> num1;
+    std::cin >> num2;
+    std::cin >> num3;
+    std::cout << num1 << "  " << num2 << "  " << num3;
+
+    return 0;
+}
