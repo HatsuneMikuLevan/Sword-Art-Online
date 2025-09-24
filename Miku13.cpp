2940a14a54a05968fb1c23f49763f5540d7dbc31
Miku Hatsune <game.app13579@gmail.com>
2025-09-24 20:55:22 +0300

    Add files via upload

---
+++ Miku13.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+
+int main() {
+
+    int number;
+
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Введите целое число: ";
+    std::cin >> number;
+    int a = number + 1;
+    int b = number - 1;
+    std::cout << "Следующее за числом " << number << " число – " << a << ".";
+    std::cout << "Для числа " << number << " предыдущее число – " << b << ".";
+
+
+
+    return 0;
+
+}
