5586a54301813f1a68b874aa182fb5983cc8c2b4
Miku Hatsune <game.app13579@gmail.com>
2025-09-26 13:34:16 +0300

    Create Miku7.cpp

---
+++ Miku7.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  
+#include <iomanip>  
+  
+int main() {  
+    double pi = 2.71828;  
+    std::cout << std::fixed << std::setprecision(1);
+    std::cout << pi;
+      
+    return 0;  
+}  
