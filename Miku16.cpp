cf9484e22c06abe0a0640d276b7a21d682c20b9d
Miku Hatsune <game.app13579@gmail.com>
2025-09-28 15:56:30 +0300

    Create Miku16.cpp

---
+++ Miku16.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    std::cout <<"а)"<< "5 10" << " " <<"б)"<< "100 t" << " " <<" в)"<< "x 25" << std::endl;
+    std::cout <<"  "<< "7 см" << " " <<"  "<< "1949 v" << " " <<"  "<< "x y" << std::endl;
+   
+    return 0;
+}
