54b21b3019eb344254c174ec62930ce98d0761e5
Miku Hatsune <game.app13579@gmail.com>
2025-09-28 17:22:34 +0300

    Create Miku22.cpp

---
+++ Miku22.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+int main() {
+    double radius; 
+    std::cout << "Введите радиус окружности: "; 
+    std::cin >> radius; 
+    double diameter = 2 * radius; 
+    std::cout << "Диаметр окружности: " << diameter ; 
+
+    
+}
