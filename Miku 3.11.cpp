24d13951eee4f50ddf456fecb661b88aa7313a9f
Miku Hatsune <game.app13579@gmail.com>
2025-10-05 20:24:57 +0300

    Create Miku 3.11.cpp

---
+++ Miku 3.11.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int n; 
+    std::cout << "Введите количество прошедших месяцев: ";
+    std::cin >> n;
+    int x = n + 1;
+    std::cout << "Значение x: " << x << std::endl;
+
+    return 0;
+}
