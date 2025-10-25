a64ad9d59e782989617c81eb053ccf2b4cad2882
Miku Hatsune <game.app13579@gmail.com>
2025-10-25 13:53:12 +0300

    Update Miku 4.27.cpp

--- Miku 4.27.cpp
+++ Miku 4.27.cpp
@@ -1,23 +1,16 @@
-#include <iostream>
-
-void solve_4_26(int n) {
-    int d1 = n / 10;
-    int d2 = n % 10;
-    std::cout << "а) ";
-    if (d1 > d2) {
-        std::cout << "Первая цифра больше" << std::endl;
-    } else if (d2 > d1) {
-        std::cout << "Вторая цифра больше" << std::endl;
-    } else {
-        std::cout << "Цифры равны" << std::endl;
-    }
-    std::cout << "б) " << (d1 == d2 ? "Да" : "Нет") << std::endl;
+#include #include
+void solve_4_27(int n) {
+int d1 = n / 10;
+int d2 = n % 10;
+long long square_n = static_cast(n) * n;
+long long sum_cubes = pow(d1, 3) + pow(d2, 3);
+std::cout << (square_n == 4 * sum_cubes ? "Да" : "Нет") << std::endl;
 }
 
 int main() {
-    int n;
-    std::cout << "Введите двузначное число: ";
-    std::cin >> n;
-    solve_4_26(n);
-    return 0;
+int n;
+std::cout << "Введите двузначное число: ";
+std::cin >> n;
+solve_4_27(n);
+return 0;
 }
