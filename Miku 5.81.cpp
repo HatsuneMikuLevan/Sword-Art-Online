09b085911f2ded1d90fdf4e220906fb40621e4d2
Miku Hatsune <game.app13579@gmail.com>
2025-11-04 16:53:32 +0300

    Update Miku 5.81.cpp

--- Miku 5.81.cpp
+++ Miku 5.81.cpp
@@ -11,3 +11,18 @@ int main() {
     std::cout << std::endl;
     return 0;
 }
+#include <iostream>
+
+int main() {
+    std::cout << "б) Трехзначные числа, кратные 7 с суммой цифр, кратной 7:" << std::endl;
+    for (int i = 100; i <= 999; ++i) {
+        if (i % 7 == 0) {
+            int sum_digits = (i / 100) + ((i / 10) % 10) + (i % 10);
+            if (sum_digits % 7 == 0) {
+                std::cout << i << " ";
+            }
+        }
+    }
+    std::cout << std::endl;
+    return 0;
+}
