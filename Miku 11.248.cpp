bf4da09e286c3773fa914f34838d17db937284d3
Miku Hatsune <game.app13579@gmail.com>
2026-01-06 15:31:15 +0300

    Create Miku 11.248.cpp

---
+++ Miku 11.248.cpp
@@ -0,0 +1,28 @@
+
+
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> a = {1, 2, 3, 4, 5, 6};
+    int size = a.size();
+    std::vector<int> b(size);
+
+    for (int i = 0; i < size; ++i) {
+        if (a[i] % 2 == 0) {
+            b[i] = a[i] * 2;
+        } else {
+            b[i] = a[i];
+        }
+    }
+
+    std::cout << "Array a: ";
+    for (int val : a) std::cout << val << " ";
+    std::cout << std::endl;
+
+    std::cout << "Array b: ";
+    for (int val : b) std::cout << val << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
