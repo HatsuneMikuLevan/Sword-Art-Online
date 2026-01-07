2af6efa70d1316a8d8e83203cc02f25c6b52f68d
Miku Hatsune <game.app13579@gmail.com>
2026-01-07 21:37:11 +0300

    Create Miku 12.36.cpp

---
+++ Miku 12.36.cpp
@@ -0,0 +1,24 @@
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string word;
+    cout << "Введите слово из 12 букв: ";
+    cin >> word;
+
+    if (word.length() == 12) {
+        string part1 = word.substr(0, 4);   // первая треть
+        string part2 = word.substr(4, 4);   // вторая треть
+        string part3 = word.substr(8, 4);   // третья треть
+
+        string result = part2 + part3 + part1; // 2 -> 1, 3 -> 2, 1 -> 3
+        cout << "Результат (a): " << result << endl;
+    } else {
+        cout << "Слово должно содержать ровно 12 букв." << endl;
+    }
+
+    return 0;
+}
